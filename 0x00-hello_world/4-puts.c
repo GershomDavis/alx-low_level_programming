1fb2c2583006bbd20248f172e7bb6d227e2f8af5
Gershom Davis <gershdavis94@gmail.com>
2022-04-07 14:12:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,20 @@
 
 /**
  *
- *  * main - entry point
+ * main - entry point
  *
- *   *
  *
- *    * Return: 0 after printing the function
  *
- *     */
+ * Return: 0 after printing the function
+ *
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+return (0);
 
 }
